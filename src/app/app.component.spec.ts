df4c2aa036500770f6aad97afe07907f8c28b200
mayinbun <bunyamin.gs@gmail.com>
2021-03-12 13:27:59 +0100

    ng8 tsconfig changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,12 +3,12 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    beforeEach(async(() => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [AppComponent],
             schemas: [NO_ERRORS_SCHEMA],
-        }).compileComponents();
-    }));
+        });
+    });
 
     it('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
