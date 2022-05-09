5f50849ec2dc89a63b3b68a053ad048cf8d9f64d
idsme <idsmesales@gmail.com>
2022-05-09 23:13:50 +0200

    /feature/added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import {CommonModule} from '@angular/common';
 import { StraigtForwardSolutionChallengeComponent } from './straigt-forward-solution-challenge3/straigt-forward-solution-challenge.component';
 import { RxjsSolutionBasicChallengeComponent } from './rxjs-solution-basic-challenge/rxjs-solution-basic-challenge.component';
 import { RxjsSolutionAdvancedChallenge3Component } from './rxjs-solution-advanced-challenge3/rxjs-solution-advanced-challenge3.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-    declarations: [AppComponent, StraigtForwardSolutionChallengeComponent, StraigtForwardSolutionChallengeComponent, RxjsSolutionBasicChallengeComponent, RxjsSolutionAdvancedChallenge3Component],
+    declarations: [AppComponent, StraigtForwardSolutionChallengeComponent, StraigtForwardSolutionChallengeComponent, RxjsSolutionBasicChallengeComponent, RxjsSolutionAdvancedChallenge3Component, HeaderComponent],
     imports: [BrowserModule, CommonModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
     providers: [],
     bootstrap: [AppComponent],
